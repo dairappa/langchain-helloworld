da64f97e64d7db5a145c1f7e4a626af7ac2a5c11
MAEKAWA Hiroshi <hiroshi1.maekawa@daikin.co.jp>
2023-03-03 08:32:46 +0900

    agentの試用コードを追加

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,47 @@
 import { OpenAI } from "langchain";
 import { PromptTemplate } from "langchain/prompts";
 import { LLMChain } from "langchain/chains";
-
+import { initializeAgentExecutor } from "langchain/agents";
+import { SerpAPI, Calculator } from "langchain/tools";
 
 const model = new OpenAI({ temperature: 0.9 });
+await tryAgent();
+await tryPromptTemplate();
 
-const template = "What is a good name for a company that makes {product}?";
-const prompt = new PromptTemplate({
-  template: template,
-  inputVariables: ["product"],
-});
+async function tryAgent() {
+    console.log("try agent: start");
 
-const chain = new LLMChain({ llm: model, prompt: prompt });
+    const tools = [new SerpAPI(), new Calculator()];
+    const executor = await initializeAgentExecutor(
+        tools,
+        model,
+        "zero-shot-react-description"
+    );
+    console.log("Loaded agent.");
 
-const res = await chain.call({ product: "colorful socks" });
-console.log(res);
+    const input = "Who is Olivia Wilde's boyfriend? What is his current age raised to the 0.23 power?";
+    console.log(`Executing with input "${input}"...`);
+
+    const result = await executor.call({ input });
+    console.log(`Got output ${result.output}`);
+
+    console.log("try agent: end");
+}
+
+async function tryPromptTemplate() {
+    console.log("try prompt template: start");
+
+    const template = "What is a good name for a company that makes {product}?";
+    const prompt = new PromptTemplate({
+        template: template,
+        inputVariables: ["product"],
+    });
+
+    const chain = new LLMChain({ llm: model, prompt: prompt });
+
+    const res = await chain.call({ product: "colorful socks" });
+    console.log(res);
+
+    console.log("try prompt template: end");
+}
 
