ee4a689ed2524757bd501d2cc41b7715273c6d4f
MAEKAWA Hiroshi <hiroshi1.maekawa@daikin.co.jp>
2023-03-03 08:40:33 +0900

    Memoryの利用追加

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
 import { OpenAI } from "langchain";
 import { PromptTemplate } from "langchain/prompts";
-import { LLMChain } from "langchain/chains";
+import { ConversationChain, LLMChain } from "langchain/chains";
 import { initializeAgentExecutor } from "langchain/agents";
 import { SerpAPI, Calculator } from "langchain/tools";
+import { BufferMemory } from "langchain/memory";
 
 const model = new OpenAI({ temperature: 0.9 });
-await tryAgent();
+// await tryAgent();
 await tryPromptTemplate();
+await tryMemory();
+
+async function tryMemory() {
+    console.log("try memory: start");
+    const memory = new BufferMemory();
+    const chain = new ConversationChain({ llm: model, memory: memory });
+    const res1 = await chain.call({ input: "Hi! I'm Jim." });
+    console.log(res1);
+    const res2 = await chain.call({ input: "What's my name?" });
+    console.log(res2);
+    console.log("try memory: end");
+}
 
 async function tryAgent() {
     console.log("try agent: start");
